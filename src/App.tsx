ec6257a42005c54c1ac0f592b3968c70ab7be4cc
KingNeoIV <michael.rios.nw@hotmail.com>
2026-05-02 23:09:24 -0500

    feat(core): 'integrate project grid into main layout below hero section'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ProjectGrid from './components/ProjectGrid'
 
 function App() {
   return (
     <div className="bg-black min-h-screen text-white selection:bg-emerald-500/30">
       <Navbar />
       <main>
-        {/* We keep pt-16 to leave room for the fixed navbar */}
+        {/* pt-16 ensures content starts below the fixed navbar */}
         <div className="pt-16">
           <Hero />
+          <ProjectGrid />
         </div>
       </main>
     </div>
